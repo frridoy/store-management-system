664fe22d99eb65eb3b5d8b7e66da1e946eb78b89
frridoy <foyzur.r.h@gmail.com>
2025-06-25 16:42:10 +0600

    updated

---
+++ app/Models/Upazila.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Upazila extends Model
+{
+    protected $table = 'upazilas';
+    protected $primaryKey = 'id';
+}
